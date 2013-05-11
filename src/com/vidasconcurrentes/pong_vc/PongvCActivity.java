27dc8de55c5fbb07d6d4414b2f9c9a10bd906b11
armandomj <armandovmj@gmail.com>
2013-05-11 17:38:29 +0100

    All the buttons have toast

--- src/com/vidasconcurrentes/pong_vc/PongvCActivity.java
+++ src/com/vidasconcurrentes/pong_vc/PongvCActivity.java
@@ -1,11 +1,15 @@
 package com.vidasconcurrentes.pong_vc;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,11 +20,11 @@ public class PongvCActivity extends Activity {
         super.onCreate(savedInstanceState);
         //Quitamo el título de la aplicación.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //Ponemos la pantalla completa.
+        //Ponemos la pantalla completa, quitamos barra de notificaciones.
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_pongv_c);
         
-    	//Creamo la variable que contiene el area que usaremos como boton de Jugar.
+    	//Creamos la variable que contiene el area que usaremos como boton de Jugar.
     	TextView play = (TextView)findViewById(R.id.play_button);
     	play.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
@@ -28,5 +32,29 @@ public class PongvCActivity extends Activity {
 			}
 		});
     	
+    	//Ahora creamos la que contiene el boton de opciones.
+    	TextView options = (TextView)findViewById(R.id.options_button);
+    	options.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), R.string.menu_options, Toast.LENGTH_SHORT).show();				
+			}
+		});
+    	
+    	//Ahora el del que contiene el boton de salir.
+    	TextView exit = (TextView)findViewById(R.id.exit_button);
+    	exit.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), R.string.menu_exit, Toast.LENGTH_SHORT).show();				
+			}
+		});
+    	
+    	//Cuando se pulsa la imagen se abre el navegador y carga la dirección de la web.
+    	ImageView logo = (ImageView)findViewById(R.id.imageView1);
+    	logo.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse("htttp://www.vidasconcurrentes.com"));
+				startActivity(viewIntent);
+			}
+		});
     }
 }
